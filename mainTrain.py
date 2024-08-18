743df41a5743a50815fdce697d0e69ef0f65241a
Nathan <reanathanbehailu@gmail.com>
2024-08-18 21:06:34 +0300

    built, fitted, compiled and saved model

--- mainTrain.py
+++ mainTrain.py
@@ -6,11 +6,14 @@ from PIL import Image
 import numpy as np
 from sklearn.model_selection import train_test_split
 from keras.utils  import normalize 
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense 
 
 image_directory = "./dataset" 
 
 dataset = []
 label = []
+INPUT_SIZE = 64
 
 def load_and_process_images(image_directory):
     no_tumor_images = os.listdir(image_directory + '/no')
@@ -24,7 +27,7 @@ def load_and_process_images(image_directory):
             if image_name.split('.')[1] == 'jpg':
                 image = cv2.imread(image_directory + f'/{label_value}/{image_name}')
                 image = Image.fromarray(image, 'RGB')
-                image = image.resize((64, 64))
+                image = image.resize((INPUT_SIZE, INPUT_SIZE))
                 dataset.append(np.array(image))
                 label.append(0 if label_value == 'no' else 1)
 
@@ -44,3 +47,35 @@ x_train, x_test, y_train, y_test = train_test_split(dataset, label, test_size = 
 x_train = normalize(x_train, axis = 1)
 x_test = normalize(x_test, axis = 1)
 
+# model building 
+model = Sequential()
+
+model.add(Conv2D(32, (3,3), input_shape = (INPUT_SIZE,INPUT_SIZE,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size = (2,2)))
+
+model.add(Conv2D(32, (3,3), kernel_initializer = 'he_uniform'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size = (2,2)))
+
+model.add(Conv2D(32, (3,3), kernel_initializer = 'he_uniform'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size = (2,2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+
+model.fit(x_train, y_train, 
+          batch_size = 16, 
+          verbose = 1, 
+          epochs = 10, 
+          validation_data = (x_test, y_test),
+          shuffle = False)
+
+model.save('BrainTumor10Epochs.h5')
